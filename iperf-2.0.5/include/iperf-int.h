564198b6eb14e730902a2c292dae08bfa130e00c
alex <alex@alex-Latitude-E5440.(none)>
2016-01-05 09:27:34 +0800

    iperf-2.0.5_64bit.git

---
+++ iperf-2.0.5/include/iperf-int.h
@@ -0,0 +1,9 @@
+#ifndef _IPERF_INCLUDE_IPERF_INT_H
+#define _IPERF_INCLUDE_IPERF_INT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "iperf 2.0.5"
+/* generated using gnu compiler aarch64-linux-gnu-gcc (Linaro GCC 4.9-2015.01-3) 4.9.3 20150113 (prerelease) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
